cdb486dc87f1a8f9c35d97ba83789cbb86424911
Shakeel Sidhu <shakeeljutt070@gmail.com>
2022-09-15 23:35:49 +0500

    first version release

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    items:1,
+    loop:false,
+    center:true,
+    margin:10,
+    URLhashListener:true,
+    autoplayHoverPause:true,
+    startPosition: 'URLHash'
+});
+
+
+$('.owl-carousel.pets-slider--inner').owlCarousel({
+    items:1,
+    loop:false,
+    center:true,
+    margin:10,
+    URLhashListener:true,
+    autoplayHoverPause:true,
+    startPosition: 'URLHash'
+});
